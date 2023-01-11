7551cfb9c9afab6b6b7fc20b53d0504bbaf6c982
kiranjha <kiranjha202@gmail.com>
2023-01-11 14:29:35 +0530

    structure concept

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -40,18 +40,54 @@
 // }
 
 //printing the structure
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 40,
+//         height: 50,
+//     };
+
+//     println!("rect1 is {:#?}", rect1);
+//     // dbg!(&rect1);
+// }
+
+/////////////////////////////////////////////////
 fn main() {
-    let rect1 = Rectangle {
-        width: 40,
-        height: 50,
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+    let mut user1 = User {
+        email: String::from("rustaceans@rapidinnovation.dev"),
+        username: String::from("Rustaceans"),
+        active: true,
+        sign_in_count: 1,
     };
+    println!("User's Email is {}",user1.email);
+    user1.email = String::from("rustacean@rapidinnovation.dev");
+    println!("User's Email is {}",user1.email);
 
-    println!("rect1 is {:#?}", rect1);
-    // dbg!(&rect1);
+    let user2 = build_user(String::from("user2@ri.dev"), String::from("Imuser2"));
+    println!("User2 username : {}, email : {}",user2.username, user2.email);
+
+    let inactive_user2 = User {
+        active: false,
+        ..user2 //rest field same as user2
+    };
+    println!("Inactive User2 Info - name : {}, emai : {}, active : {}, signInCount : {}",inactive_user2.username, inactive_user2.email, inactive_user2.active, inactive_user2.sign_in_count);
+    fn build_user(user_email: String, username: String) -> User {
+        User {
+            email: user_email,  
+            username,   // shorthand for username: username 
+            active: true,
+            sign_in_count: 1,
+        }
+    }
 }
---
+++ structure/target/debug/deps/structure-f8257257b32b33ca.d
@@ -0,0 +1,5 @@
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\structure\target\debug\deps\structure-f8257257b32b33ca.rmeta: src\main.rs
+
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\structure\target\debug\deps\structure-f8257257b32b33ca.d: src\main.rs
+
+src\main.rs:
