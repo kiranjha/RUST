7fb905d3036218b98d3e322e8a281b2c82b63b44
kiranjha <kiranjha202@gmail.com>
2023-01-12 11:47:14 +0530

    slice-assessment given in ppt

---
+++ slice-assessment/target/debug/deps/slice.d
@@ -0,0 +1,5 @@
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\slice\target\debug\deps\slice.exe: src\main.rs
+
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\slice\target\debug\deps\slice.d: src\main.rs
+
+src\main.rs:
---
+++ slice-assessment/target/debug/slice.d
@@ -0,0 +1,1 @@
+D:\Rapid\ Innovation\ join\ 16th\ Aug\ 2022\rust-learn-\slice\target\debug\slice.exe: D:\Rapid\ Innovation\ join\ 16th\ Aug\ 2022\rust-learn-\slice\src\main.rs
