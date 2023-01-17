d54cf5329b1a0f471e150c7002ec30824ded4df7
kiranjha <kiranjha202@gmail.com>
2023-01-17 18:53:09 +0530

    HashMaps

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -10,9 +10,12 @@ fn main() {
     //get method returns an option<&V>
     //handles option by calling copied() to get an Option<i32> rather than an Option<i32>
     //then, unwrap_or(0) to set scores to zero if scores doesn't have any entry for the key.
-    println!("{}",score);
+    println!("{}'s score is: {}",team_name, score);
 
     iterate_hashmap();
+    overwriting_value_in_hashmap();
+    only_insert_value_when_key_has_no_value();
+    update_value_based_on_old_value();
 }
 fn hashmap_and_ownership() {
     let field_name = String::from("Favourite color");
@@ -29,4 +32,31 @@ fn iterate_hashmap() {
     for (key, value) in &scores {
         println!("{key}: {value}");
     }
+}
+fn overwriting_value_in_hashmap() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    println!("Value before overwriting: {:?}",scores);
+    scores.insert(String::from("Blue"), 25);
+    println!("Value after overwriting: {:?}",scores);
+}
+fn only_insert_value_when_key_has_no_value() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50); 
+    //The or_insert method on Entry is defined to return a mutable reference
+    println!("{:?}",scores);
+}
+fn update_value_based_on_old_value() {
+    let text = "Hello World Wonderful World";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count =  map.entry(word).or_insert(0); //returns mutable reference which is assigned to count
+        //The split_whitespace method returns an iterator over sub-slices, separated by whitespace, of the value in text.
+        //The or_insert method returns a mutable reference (&mut V) to the value for the specified key.
+        *count += 1;
+    }
+    println!("{:?}",map);
 }
