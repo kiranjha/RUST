f14286cbcd13b88adbc55b301bce612df71bbefb
kiranjha <kiranjha202@gmail.com>
2023-01-19 18:51:37 +0530

    1st type of Error handled using panic! macro

---
+++ unrecoverable-errors/target/debug/deps/unrecoverable_errors.d
@@ -0,0 +1,5 @@
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\unrecoverable-errors\target\debug\deps\unrecoverable_errors.exe: src\main.rs
+
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\unrecoverable-errors\target\debug\deps\unrecoverable_errors.d: src\main.rs
+
+src\main.rs:
---
+++ unrecoverable-errors/target/debug/unrecoverable-errors.d
@@ -0,0 +1,1 @@
+D:\Rapid\ Innovation\ join\ 16th\ Aug\ 2022\rust-learn-\unrecoverable-errors\target\debug\unrecoverable-errors.exe: D:\Rapid\ Innovation\ join\ 16th\ Aug\ 2022\rust-learn-\unrecoverable-errors\src\main.rs
