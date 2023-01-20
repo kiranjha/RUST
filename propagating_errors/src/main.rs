37054f72f9b1ae6896bbb8f8e5bd03ae72f76bd7
kiranjha <kiranjha202@gmail.com>
2023-01-20 18:03:59 +0530

    instead of handling the error within the function itself, can return the error to the calling code so that it can decide what to do. This is known as propagating the error

--- propagating_errors/src/main.rs
+++ propagating_errors/src/main.rs
@@ -1,9 +1,18 @@
 #![allow(unused)]
-fn main() {
-    use std::fs::File;
-    use std::io::{self, Read};
-    
-    fn read_username_from_file() -> Result<String, io::Error> {
+use std::fs::{self, File};
+use std::io::{self, Read};
+use std::error::Error;
+fn main() -> Result<(), Box<dyn Error>> { //Box<dyn Error> means "any kind of error"
+
+    assert_eq!(last_char_of_first_line("Hello, world\nHow are you today?"), Some('d'));
+    assert_eq!(last_char_of_first_line(""), None);
+    assert_eq!(last_char_of_first_line("\nhi"), None);
+
+    let greeting_file = File::open("hello.txt")?;
+    Ok(())  //return ()
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
         let f = File::open("hello.txt");
 
         let mut f = match f {
@@ -17,18 +26,32 @@ fn main() {
             Ok(_) => Ok(username),
             Err(e) => Err(e),
         }
-    }
+}
 
-    fn read_username_from_file_short_way() -> Result<String, io::Error> {
+fn read_username_from_file_short_way() -> Result<String, io::Error> {
         let mut username_file = File::open("hello.txt")?;
         let mut username = String::new();
         username_file.read_to_string(&mut username)?;
         Ok(username)
-    }
+}
 
-    fn read_username_from_file_shortest_way() -> Result<String, io::Error> {
+fn read_username_from_file_shorter_way() -> Result<String, io::Error> {
         let mut username = String::new();
         File::open("hello.txt")?.read_to_string(&mut username)?;
         Ok(username)
-    }
+}
+
+fn read_username_from_file_shortest_way() -> Result<String, io::Error> { 
+        fs::read_to_string("hello.txt")
+} 
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+        //lines() method returns an iterator over the lines in the string.
+        //it calls next() method on the iterator to get the first value from the iterator. If text is empty string, this call to next will return None.
+        //in which case we use ? operator to stop and return None.
+        //if text is not empty string, next will return Some value containit a string slice of the first line in text.
+        //chars method on that string slice to get an iterator of its characters.
+        //we are interested in the last character in this first line, so we call last method to return the last item in the iterator.
+
+        text.lines().next()?.chars().last()
 }
