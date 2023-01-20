dd42862d2c88a83b9c2c0369bf8635d291b17954
kiranjha <kiranjha202@gmail.com>
2023-01-20 18:05:57 +0530

    Working on this chapter

--- generics_traits_lifetime/src/main.rs
+++ generics_traits_lifetime/src/main.rs
@@ -1,10 +1,50 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 64];
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-    println!("The largest number is {largest}");
+// fn largest_i32(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+// fn largest<T>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
 }
+fn main() {
+    // let number_list = vec![34, 50, 25, 100, 65];
+    // let result = largest(&number_list);
+    // println!("The largest number is {result}");
+
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+    // let result = largest(&char_list);
+    // println!("The largest number is {result}");
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+}
+
---
+++ generics_traits_lifetime/target/debug/deps/generics_traits_lifetime-deadcc86ada6e17f.d
@@ -0,0 +1,5 @@
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\generics_traits_lifetime\target\debug\deps\generics_traits_lifetime-deadcc86ada6e17f.rmeta: src\main.rs
+
+D:\Rapid Innovation join 16th Aug 2022\rust-learn-\generics_traits_lifetime\target\debug\deps\generics_traits_lifetime-deadcc86ada6e17f.d: src\main.rs
+
+src\main.rs:
